507b146e836afee7762ea82d06a94893e5bc2526
unknown <>
2021-07-05 18:57:43 -0700

    Search Page

--- 404.php
+++ 404.php
@@ -5,17 +5,13 @@
 
 		<main id="main" class="site-main" role="main">
 
-			<?php if ( have_posts() ) : while ( have_posts() ): the_post();  ?>
 			
-				<?php get_template_part( 'template-parts/content', 'page'); ?>
-
-			<?php endwhile; else: ?>
 
 				<?php get_template_part( 'template-parts/content', 'none');?>
 
-			<?php endif; ?>
+			
 
-			<p>Template: page.php</p>
+			<p>Template: 404.php</p>
 
 			
 
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -2,7 +2,7 @@
 
 						<header class="entry-header">
 
-							<h1><?php esc_html_e('404', 'wphierarchy') ?></h1>
+							<h1><?php esc_html_e('404 - Page Not Found', 'wphierarchy') ?></h1>
 							
 							
 
@@ -11,6 +11,7 @@
 						<div class="entry-content">
 							
 							<p><?php esc_html_e('Sorry! No Content Found.', 'wphierarchy') ?></p>
+							<p> <?php get_search_form(); ?> </p>
 						</div>
 
 
